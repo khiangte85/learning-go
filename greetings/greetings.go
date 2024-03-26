dede3e28e6b6f4bf4660030ebd0675a8e6d684a4
awmtea <awmtea105.infinity@gmail.com>
2024-03-26 11:24:19 +0700

    random format #1

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,14 +3,25 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
-	message := fmt.Sprintf("Hi, %v. Welcome", name)
+	message := fmt.Sprintf(randomFormat(), name)
 
 	if name == "" {
 		return "", errors.New("name is empty")
 	}
 
 	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string {
+		"Hi, %v, Welcome",
+		"Great to see you %v",
+		"Hail, %v!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
