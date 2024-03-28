20028ecad67f385e095c9b83bd82df5efeca05d9
awmtea <awmtea105.infinity@gmail.com>
2024-03-28 11:40:48 +0700

    investment calculator

---
+++ investment_calculator/app.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var years float64
+	var returnRate float64
+
+	fmt.Print("Enter Amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Enter years: ")
+	fmt.Scan(&years)
+	fmt.Print("Enter rate: ")
+	fmt.Scan(&returnRate)
+
+	futureAmount := investmentAmount * math.Pow(1+returnRate/100, years)
+	futureRealAmount := futureAmount / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println("Return Amount: ", futureAmount)
+	fmt.Println("Real return Amount: ", futureRealAmount)
+}
