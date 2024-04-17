faf788820dd97110eec652facd30c1f38bacaac9
awmtea <awmtea105.infinity@gmail.com>
2024-04-17 12:07:16 +0700

    array and slices

---
+++ array/go.mod
@@ -0,0 +1,3 @@
+module khiangte85/array
+
+go 1.22.1
---
+++ array/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main(){
+	array := [5]int{2,4,5,6,7}
+	fmt.Printf("%v\n", array)
+
+	var transportations []string
+	transportations = append(transportations, "Car")
+	transportations = append(transportations, "Bus")
+	fmt.Printf("%v\n", transportations)
+
+	fmt.Printf("%v\n", transportations[1])
+	
+	transportations = append(transportations, "Aeroplane")
+	transportations = append(transportations, "Jet")
+	transportations = append(transportations, "Rocket")
+
+	landTransportations :=  transportations[:2]
+	airTransportations := transportations[2:]
+
+	fmt.Println(transportations)
+	fmt.Println(len(transportations), cap(transportations))
+	fmt.Println(landTransportations)
+	fmt.Println(len(landTransportations), cap(landTransportations))
+	fmt.Println(airTransportations)
+	fmt.Println(len(airTransportations), cap(airTransportations))
+
+	practice()
+}
